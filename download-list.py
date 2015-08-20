226e8afb473c9b96337d51db03d47b45429cafc8
Francois Marier <francois@mozilla.com>
2015-08-19 17:06:17 -0700

    Spoof the user agent

--- download-list.py
+++ download-list.py
@@ -10,6 +10,10 @@ list = b'mozpub-track-digest256'
 
 listUrl = baseUrl + 'downloads?client=%s&appver=%s&pver=2.2' % (clientId, appVersion)
 data = list + b';'
+headers = {
+  "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:42.0) Gecko/20100101 Firefox/42.0"
+}
 
-f= urllib.request.urlopen(listUrl, data=data)
+request = urllib.request.Request(listUrl, data=data, headers=headers)
+f = urllib.request.urlopen(request)
 print(f.read().decode('ISO8859-1'))

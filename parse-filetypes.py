33f5bb8cb28fe62c059d4497751627ea266f929d
Francois Marier <francois@mozilla.com>
2016-08-12 13:11:51 -0700

    Display the entire contents of the .pb file

--- parse-filetypes.py
+++ parse-filetypes.py
@@ -5,13 +5,51 @@
 
 import argparse
 import os
-import struct
-import sys
 import download_file_types.download_file_types_pb2 as pb2
 
 
+extensions = []
+
+
+def process_platform_settings(extension, ping, settings):
+    platform = "any"
+    if settings.platform == pb2.DownloadFileType.PLATFORM_ANDROID:
+        platform = "android"
+    elif settings.platform == pb2.DownloadFileType.PLATFORM_CHROME_OS:
+        platform = "chromeos"
+    elif settings.platform == pb2.DownloadFileType.PLATFORM_LINUX:
+        platform = "linux"
+    elif settings.platform == pb2.DownloadFileType.PLATFORM_MAC:
+        platform = "mac"
+    elif settings.platform == pb2.DownloadFileType.PLATFORM_WINDOWS:
+        platform = "windows"
+
+    danger_level = "not"
+    if settings.danger_level == pb2.DownloadFileType.ALLOW_ON_USER_GESTURE:
+        danger_level = "gesture"
+    elif settings.danger_level == pb2.DownloadFileType.DANGEROUS:
+        danger_level = "YES"
+
+    print "[%s] %s (danger=%s, ping=%s)" % (platform, extension, danger_level, ping)
+
+
 def process_file_type(file_type):
-    print file_type.extension
+    extension = file_type.extension
+    extensions.append(extension)
+    if len(file_type.platform_settings) != 1:
+        print "WARNING: %s (%d != 1 platform settings)"\
+            % (extension, len(file_type.platform_settings))
+    if file_type.is_archive:
+        extension += "[a]"
+
+    ping = "sampled"
+    if file_type.ping_setting == pb2.DownloadFileType.NO_PING:
+        ping = "no"
+    elif file_type.ping_setting == pb2.DownloadFileType.FULL_PING:
+        ping = "FULL"
+
+    for platform_setting in file_type.platform_settings:
+        process_platform_settings(extension, ping, platform_setting)
 
 
 def process_config(protobuffile, verbose, quiet):
@@ -21,8 +59,21 @@ def process_config(protobuffile, verbose, quiet):
             print "Parsing a %s-byte file" % len(binary_string)
         config = pb2.DownloadFileTypeConfig()
         config.ParseFromString(binary_string)
+
+        default_file_type = config.default_file_type
+        print "\nDefaults:\n--------------------\n%s--------------------\n"\
+            % default_file_type
+
         for file_type in config.file_types:
             process_file_type(file_type)
+
+
+def dump_extensions():
+    print ""
+    extensions.sort()
+    for extension in extensions:
+        print "StringEndsWith(fileName, NS_LITERAL_STRING(\".%s\")) ||"\
+            % extension
 
 
 def main():
@@ -37,7 +88,7 @@ def main():
     args = parser.parse_args()
 
     if not os.path.isfile(args.protobuffile):
-        print "Error: '%s' not found" % args.protobuffile # TODO: file=sys.stderr
+        print "Error: '%s' not found" % args.protobuffile
         return False
 
     if args.quiet and args.verbose:
@@ -45,6 +96,7 @@ def main():
         return False
 
     process_config(args.protobuffile, args.verbose, args.quiet)
+    # dump_extensions()
 
     return True
 

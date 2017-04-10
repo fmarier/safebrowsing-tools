fbc08409b110dcee0dc565f81dae9cb244975524
Francois Marier <francois@mozilla.com>
2017-04-10 15:31:55 -0700

    Add support for requesting more than one list at a time

--- download-list.py
+++ download-list.py
@@ -8,16 +8,21 @@ key = ''
 
 baseUrl = 'https://shavar.services.mozilla.com/'
 #baseUrl = 'https://shavar.stage.mozaws.net/'
-list = b'mozstd-track-digest256'
+lists = 'base-track-digest256'
+lists = 'base-track-digest256,mozstd-trackwhite-digest256,mozplugin-block-digest256,allow-flashallow-digest256,except-flashallow-digest256,block-flash-digest256,except-flash-digest256,block-flashsubdoc-digest256,except-flashsubdoc-digest256'
 
 #baseUrl = 'https://safebrowsing.google.com/safebrowsing/'
-#list = b'googpub-phish-shavar'
+#lists = 'googpub-phish-shavar'
 
-listUrl = baseUrl + 'downloads?client=%s&appver=%s&pver=2.2&key=%s' % (clientId, appVersion, key)
-data = list + b';'
+data = b''
+for list in lists.split(','):
+    data += list.encode('ascii') + b';\n'
+
 headers = {
   "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:45.0) Gecko/20100101 Firefox/45.0"
 }
+
+listUrl = baseUrl + 'downloads?client=%s&appver=%s&pver=2.2&key=%s' % (clientId, appVersion, key)
 
 request = urllib.request.Request(listUrl, data=data, headers=headers)
 f = urllib.request.urlopen(request)
